95d64cf2fad59caedd2973c9838666400a472f9e
German Romarion <grromarion@hotmail.com>
2014-09-28 11:29:07 -0300

    Began to implement functions to obtain B and C with n as parameter

--- B.m
+++ B.m
@@ -1,8 +1,13 @@
-function [B] = B(n)
+function B = B(n)
 	B = eye(n + 1);
 	B(n + 1, n + 1) = 0;
+	B = h(n)^2;
 end
 
-function [h] = h(n)
+function h = h(n)
 	h = 1 / (n + 1);
+end
+
+function C = C(A, B)
+	C = (A * B)^-1;
 end
