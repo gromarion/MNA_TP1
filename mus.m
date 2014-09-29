7475d7d88a32cad3f763dd10a5fdab76950bec97
German Romarion <grromarion@hotmail.com>
2014-09-29 18:56:24 -0300

    finished item b)

--- mus.m
+++ mus.m
@@ -1,13 +1,18 @@
-function mus = mus(n, gamma)
-	mus = 1 : 2 * n + 1;
+function mus = mus(n, gamma, limit)
+	mus = 1 : 4 * limit + 2;
 	sigma_plus = 1 / gamma + sqrt(1 / gamma^2 - 1);
 	sigma_minus = 1 / gamma - sqrt(1 / gamma^2 - 1);
-	ln_sigma = log(sigma);
-	ln_sigma_2 = ln_sigma^2;
+	ln_sigma_plus = log(sigma_plus);
+	ln_sigma_minus = log(sigma_minus);
+	ln_sigma_plus_2 = ln_sigma_plus^2;
+	ln_sigma_minus_2 = ln_sigma_minus^2;
 
-	for k = 2 : 2 : n
-		mus(k) = - 1 / (((2 * (k - 1)  + 1)^2 * pi^2 - ln_sigma_2) - ((2 * (k - 1) + 1) * 2 * pi * ln_sigma) * i);
-		mus(k + 1) = - 1 / (((2 * (-(k - 1)) + 1)^2 * pi^2 - ln_sigma_2) - ((2 * (-(k - 1)) + 1) * 2 * pi * ln_sigma) * i);
+	mus(1) = -1 / ((pi^2 - ln_sigma_plus_2) - (2 * pi * ln_sigma_plus) * i);
+	mus(2) = -1 / ((pi^2 - ln_sigma_minus_2) - (2 * pi * ln_sigma_minus) * i);
+	for k = 1 : limit
+		mus(4*k - 1) = - 1 / (((2 * k  + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_plus) * i);
+		mus(4*k) = - 1 / (((2 * k  + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_minus) * i);
+		mus(4*k + 1) = - 1 / (((2 * (-k) + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_plus) * i);
+		mus(4*k + 2) = - 1 / (((2 * (-k) + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_minus) * i);
 	end
-	mus(1) = -1 / ((pi^2 - ln_sigma_2) - (2 * pi * ln_sigma) * i);
 end
