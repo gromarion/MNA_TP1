48b1f2d4624b767c4f0b53b7ed14609a22815c58
German Romarion <grromarion@hotmail.com>
2014-09-28 21:08:28 -0300

    mus are 1/mu^2 now

--- mus.m
+++ mus.m
@@ -5,8 +5,8 @@ function mus = mus(n, gamma)
 	ln_sigma_2 = ln_sigma^2;
 
 	for k = 2 : 2 : n
-		mus(k) = sqrt(((2 * (k - 1)  + 1)^2 * pi^2 - ln_sigma_2) - ((2 * (k - 1) + 1) * 2 * pi * ln_sigma) * i);
-		mus(k + 1) = sqrt(((2 * (-(k - 1)) + 1)^2 * pi^2 - ln_sigma_2) - ((2 * (-(k - 1)) + 1) * 2 * pi * ln_sigma) * i);
+		mus(k) = - 1 / (((2 * (k - 1)  + 1)^2 * pi^2 - ln_sigma_2) - ((2 * (k - 1) + 1) * 2 * pi * ln_sigma) * i);
+		mus(k + 1) = - 1 / (((2 * (-(k - 1)) + 1)^2 * pi^2 - ln_sigma_2) - ((2 * (-(k - 1)) + 1) * 2 * pi * ln_sigma) * i);
 	end
-	mus(1) = sqrt((pi^2 - ln_sigma_2) - (2 * pi * ln_sigma) * i);
+	mus(1) = -1 / ((pi^2 - ln_sigma_2) - (2 * pi * ln_sigma) * i);
 end
