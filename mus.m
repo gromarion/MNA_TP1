efccadc9703074536523d9a12915b2ef822dec44
German Romarion <grromarion@hotmail.com>
2014-10-01 23:10:43 -0300

    further bugfixes

--- mus.m
+++ mus.m
@@ -13,8 +13,8 @@ function mus = mus(n, gamma, limit)
 	mus(4) = sqrt((pi^2 - ln_sigma_minus_2) - (2 * pi * ln_sigma_minus) * i);
 	l = 5;
 	for k = 1 : limit
-		for j = l : l + 1
-			[mu1, mu2, mu3, mu4] = partial_mus(k, mod(l, 2), ln_sigma_plus, ln_sigma_minus, ln_sigma_plus_2, ln_sigma_minus_2);
+		for j = 0 : 1
+			[mu1, mu2, mu3, mu4] = partial_mus(k, mod(j, 2), ln_sigma_plus, ln_sigma_minus, ln_sigma_plus_2, ln_sigma_minus_2);
 			mus(l) = mu1;
 			mus(l + 1) = mu2;
 			mus(l + 2) = mu3;
@@ -24,9 +24,9 @@ function mus = mus(n, gamma, limit)
 	end
 end
 
-function [mu1, mu2, mu3, mu4] = partial_mus(k, l, ln_sigma_plus, ln_sigma_minus, ln_sigma_plus_2, ln_sigma_minus_2)
-	mu1 = (-1)^l * sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_plus) * i);
-	mu2 = (-1)^l * sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_minus) * i);
-	mu3 = (-1)^l * sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_plus) * i);
-	mu4 = (-1)^l * sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_minus) * i);
+function [mu1, mu2, mu3, mu4] = partial_mus(k, j, ln_sigma_plus, ln_sigma_minus, ln_sigma_plus_2, ln_sigma_minus_2)
+	mu1 = (-1)^j * sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_plus) * i);
+	mu2 = (-1)^j * sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_minus) * i);
+	mu3 = (-1)^j * sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_plus) * i);
+	mu4 = (-1)^j * sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_minus) * i);
 end
