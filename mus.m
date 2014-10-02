5181ff5e4d84dd88892400a48eee9ed2c40ffc62
German Romarion <grromarion@hotmail.com>
2014-10-01 21:23:20 -0300

    mus were calculated in the wrong way...

--- mus.m
+++ mus.m
@@ -1,5 +1,5 @@
 function mus = mus(n, gamma, limit)
-	mus = 1 : 4 * limit + 2;
+	mus = zeros(1, 8 * limit + 4);
 	sigma_plus = 1 / gamma + sqrt(1 / gamma^2 - 1);
 	sigma_minus = 1 / gamma - sqrt(1 / gamma^2 - 1);
 	ln_sigma_plus = log(sigma_plus);
@@ -7,12 +7,18 @@ function mus = mus(n, gamma, limit)
 	ln_sigma_plus_2 = ln_sigma_plus^2;
 	ln_sigma_minus_2 = ln_sigma_minus^2;
 
-	mus(1) = -1 / ((pi^2 - ln_sigma_plus_2) - (2 * pi * ln_sigma_plus) * i);
-	mus(2) = -1 / ((pi^2 - ln_sigma_minus_2) - (2 * pi * ln_sigma_minus) * i);
+	mus(1) = sqrt((pi^2 - ln_sigma_plus_2) - (2 * pi * ln_sigma_plus) * i);
+	mus(2) = sqrt((pi^2 - ln_sigma_minus_2) - (2 * pi * ln_sigma_minus) * i);
+	mus(3) = - sqrt((pi^2 - ln_sigma_plus_2) - (2 * pi * ln_sigma_plus) * i);
+	mus(4) = - sqrt((pi^2 - ln_sigma_minus_2) - (2 * pi * ln_sigma_minus) * i);
 	for k = 1 : limit
-		mus(4*k - 1) = - 1 / (((2 * k  + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_plus) * i);
-		mus(4*k) = - 1 / (((2 * k  + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_minus) * i);
-		mus(4*k + 1) = - 1 / (((2 * (-k) + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_plus) * i);
-		mus(4*k + 2) = - 1 / (((2 * (-k) + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_minus) * i);
+		mus(8*k - 3) = sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_plus) * i);
+		mus(8*k - 2) = sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_minus) * i);
+		mus(8*k - 1) = sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_plus) * i);
+		mus(8*k) = sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_minus) * i);
+		mus(8*k + 1) = - sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_plus) * i);
+		mus(8*k + 2) = - sqrt(((2 * k  + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * k + 1) * 2 * pi * ln_sigma_minus) * i);
+		mus(8*k + 3) = - sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_plus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_plus) * i);
+		mus(8*k + 4) = - sqrt(((2 * (-k) + 1)^2 * pi^2 - ln_sigma_minus_2) - ((2 * (-k) + 1) * 2 * pi * ln_sigma_minus) * i);
 	end
 end
